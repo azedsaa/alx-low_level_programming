c4ac220395da0f6bb54b9834791b6104addfce58
root <azed.dev@hotmail.com>
2023-03-16 19:22:59 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include<stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art,
+ * followed by a new line
+ * Return: always 0.
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
