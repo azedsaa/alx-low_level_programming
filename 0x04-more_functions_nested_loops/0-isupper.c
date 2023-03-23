d96992e28ae43eefe0e3a016d3e8f73e5c89fc36
root <azed.dev@hotmail.com>
2023-03-23 16:51:40 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * _isupper - function is the letter uppercase
+  * @c: the carachter to print
+  * Return: on success 1.
+  */
+
+int _isupper(int c)
+{
+{
+	if (isupper(c))
+		return (1);
+}
+{
+	else
+		return (0);
+}
+}
