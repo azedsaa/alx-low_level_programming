8961ae3ea444a0846717b22f04ee5b91dffadfa2
root <azed.dev@hotmail.com>
2023-03-25 04:32:17 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - function print on digit  numbers
+  * @x: base 10
+  * Return: always 0.
+  */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+{
+		printf("%d\n", x);
+		x++;
+}
+	return (0);
+}
