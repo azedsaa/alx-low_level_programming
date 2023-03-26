ac642b37623b232cc62e6fda20b88d785f5a7d16
root <azed.dev@hotmail.com>
2023-03-26 04:12:21 -0700

    task 1

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,1 +1,17 @@
-#include "main.h"
+#include"main.h"
+/**
+* main - function prints all alphabets in lower case
+* Return: always 0.
+*/
+int main(void)
+{
+	int x = 'a';
+
+	while (x >= 'z')
+	{
+	_putchar(x);
+	x++;
+	}
+	_putchar('\n')
+	return (0);
+}
