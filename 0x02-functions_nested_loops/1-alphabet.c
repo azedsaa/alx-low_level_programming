a8e17d516acda49844b50a4f11088d95213e03d9
root <azed.dev@hotmail.com>
2023-03-26 06:43:45 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
   * main - function prints all alphabets in lower case
   * Return: always 0.
   */
-int main(void)
+void print_alphabet(void)
 {
 	int x = 'a';
 
@@ -13,5 +13,4 @@ int main(void)
 		x++;
 	}
 	_putchar('\n');
-	return (0);
 }
