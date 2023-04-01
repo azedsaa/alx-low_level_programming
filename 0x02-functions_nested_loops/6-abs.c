6b41c3d123a78a0408ec5c0550951f35e7d1381d
root <azed.dev@hotmail.com>
2023-04-01 15:47:28 -0700

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   * _abs - computes the absolute value of an integer.
-  * @int: number to be checked.
+  * @n: number to be checked.
   * Return: alawys 0.
 */
 int _abs(int n)
