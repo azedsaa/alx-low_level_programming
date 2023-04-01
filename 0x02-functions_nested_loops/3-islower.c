8fa54ea2f78e6bc32e3b71d0437d1139ccec0f66
root <azed.dev@hotmail.com>
2023-04-01 06:55:37 -0700

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _islower - checks if  a letter in lower case.
+  * @c: alphabet letter.
+  * Return: c if lower case 0 otherwise.
+  */
+int _islower(int c)
+{
+	if (c <= 'a' && c >= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
