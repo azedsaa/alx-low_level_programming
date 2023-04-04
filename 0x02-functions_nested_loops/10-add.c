e10360fd2552ca834ea1aee47c97bab6f61c63ec
root <azed.dev@hotmail.com>
2023-04-04 00:09:04 -0700

    task 1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+  * add - adds two integers and returns the result.
+  * @int : values to be added
+  * Return: result
+  */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+	return (result);
+}
